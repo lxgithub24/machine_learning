094477c31adac7cc49af1e4bae766d9062d3677d
刘祥 <xiang.liu@ymm56.com>
2019-08-23 20:34:43 +0800

    crf

---
+++ bilstm_crf/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2019/8/23 14:15
+# @Author  : RIO
+# @desc: TODO:DESC
--- test/test.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2019/8/16 9:37
-# @Author  : RIO
-# @desc: TODO:DESC
-
-a = '111'
-print(a[1:])
